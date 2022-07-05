819011c83d4c29be9059a434574f28e505013130
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-07-05 01:30:34 -0300

    Initializing project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import theme from '../styles/theme';
+import GlobalStyles from '../styles/globals';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <GlobalStyles />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
---
+++ styles/theme.js
@@ -0,0 +1,14 @@
+const theme = {
+  primary: '#FF921E',
+  secondary: '#1A1A1A',
+  third: '#292929',
+  gradient: 'linear-gradient(225deg, #313860 0%, #11172B 100%);',
+  grey: '#F9F9F9',
+  greysecond: '#E5E5E5',
+  greythird: '#909090',
+  white: '#FFFFFF',
+  error: '#D45D5D',
+  success: '#6fd45d'
+};
+
+export default theme;
